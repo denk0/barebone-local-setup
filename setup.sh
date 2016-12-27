79ce1ef16b12084a9ef20748164b436a827142e6
Vlad Romanenko <vladyslav@beetroot.se>
2016-12-27 14:36:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ chmod -R 777 mysql/ sites/
 mv install.sh sites/
 chmod +x sites/install.sh
 docker run --name mysql -v $PWD/mysql:/var/lib/mysql  -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
-docker run --name myadmin -d --link mysql:db -p 8888:80 phpmyadmin/phpmyadmin
+docker run --name phpmyadmin -d --link mysql:db -p 8888:80 phpmyadmin/phpmyadmin
