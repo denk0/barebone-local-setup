22e7002d99aa7c5be3ad9e5934c2e6288df84aaa
Vlad Romanenko <vladyslav@beetroot.se>
2017-01-23 11:38:49 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir mysql sites
-chmod -R 777 mysql/ sites/
-mv install.sh sites/
-chmod +x sites/install.sh
-docker run --name mysql -v $PWD/mysql:/var/lib/mysql  -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
